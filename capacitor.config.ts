19ffeb6ea39b2a4fbb981286faf571e4cc9c204b
x1emafra <x1emafra@gmail.com>
2026-03-30 04:36:11 -0300

    fix: frontend working + tailwind + capacitor sync

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,15 +2,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'task-frontend',
-  webDir: 'dist',
-  server: {
-    url: 'http://10.151.128.39:5173',
-    cleartext: true
-  },
-  android: {
-    allowMixedContent: true
-  }
+  appName: 'task-app',
+  webDir: 'dist'
 };
 
 export default config;
